59dd071ce6f0d437f258af64659d3f7fc9b68051
psahir <psahir289@gmail.com>
2024-07-15 07:32:13 -0700

    totalamount added

--- payout-front/src/App.js
+++ payout-front/src/App.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DataTable from './components/table/DataTable';
+import { RefreshIcon } from './utils/utils';
 
 function App() {
   const [file, setFile] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [responses, setResponses] = useState([]);
   const [fileData, setFileData] = useState([])
-
+  const [totalAmount, setTotalAmount] = useState(null)
   // password
   const password = "admin@123"
   const [pass, setPass] = useState(false);
@@ -51,13 +52,11 @@ function App() {
     } finally {
       setIsLoading(false); // Set loading state back to false
       const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/get-res`);
-      console.log("🚀 ~ handleSubmit ~ response:", response)
       setFileData(response.data)
     }
   };
 
   const changeHandler = (e) => {
-    console.log("🚀 ~ changeHandler ~ e:", e.target.value)
     const pass = e.target.value;
     if (password === pass) {
       setPass(true)
@@ -65,41 +64,61 @@ function App() {
 
   }
 
+  const fetchTotalAmountData = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/get-res-success-pending`);
+      setTotalAmount(response?.data?.totalAmount)
+    } catch (error) {
+      console.error('Error fetching initial data:', error);
+    }
+  };
+
+  const handleTotalAmount=()=>{
+    fetchTotalAmountData()
+  }
+
+
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/get-res`);
-        console.log("🚀 ~ useEffect ~ response:", response);
-        setFileData(response.data)
+        setFileData(response?.data)
       } catch (error) {
         console.error('Error fetching initial data:', error);
       }
     };
 
     fetchData();
+    fetchTotalAmountData();
   }, []);
 
   return (
     <>
-
-
       {pass ?
-
         <>
           <div className='flex justify-center mt-10 font-serif font-bold text-3xl'>
             <p>Add file here</p>
           </div>
           <div className='flex justify-center mt-10'>
-            <form onSubmit={handleSubmit}>
-              <input type='file' onChange={handleFileChange} />
-              <button
-                type='submit'
-                className='mx-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                disabled={isLoading} // Disable the button when isLoading is true
-              >
-                {isLoading ? 'Uploading...' : 'Upload'}
-              </button>
-            </form>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <input type='file' className='w-52' onChange={handleFileChange} />
+                <button
+                  type='submit'
+                  className='mx-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                  disabled={isLoading} // Disable the button when isLoading is true
+                >
+                  {isLoading ? 'Uploading...' : 'Upload'}
+                </button>
+              </form>
+            </div>
+
+            <div className='flex items-center bg-gray-100 px-2 cursor-pointer hover:bg-gray-300 rounded-md' onClick={handleTotalAmount}>
+              <p className='font-bold text-center'>Total amount (SUCCESS+PENDING)</p>
+              <span className='px-2'><RefreshIcon/> </span>
+              <span>{'  : '}{totalAmount}</span>
+            </div>
           </div>
           <div className='p-10'>
             <DataTable fileData={fileData} />
